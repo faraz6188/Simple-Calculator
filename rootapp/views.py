64a044cc1951502d7815ff03d91e7a3211ad6932
faraz6188 <faraz78553@gmail.com>
2021-06-17 19:09:22 +0530

    First version of Calculator application moved into github

---
+++ rootapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def submitquery(request):
+    q = request.GET['query']
+    Jsondict = {
+        "q" : q
+    }
+    return JsonResponse(Jsondict)
+    
